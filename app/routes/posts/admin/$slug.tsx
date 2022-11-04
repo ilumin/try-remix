7aab7b765a6e2ee6ff36f093e92b674a2a6cc5eb
O Teerasak Vichadee <iolumin@gmail.com>
2022-11-04 11:03:18 +0700

    fix type

--- app/routes/posts/admin/$slug.tsx
+++ app/routes/posts/admin/$slug.tsx
@@ -3,10 +3,15 @@ import type { ActionFunction, LoaderFunction } from '@remix-run/server-runtime';
 import { json } from '@remix-run/server-runtime';
 import { redirect } from '@remix-run/server-runtime';
 import invariant from 'tiny-invariant';
+import type { Post } from '~/models/post.server';
 import { createPost, updatePost, getPost, deletePost } from '~/models/post.server';
 import { requireAdminUser } from '~/session.server';
 
 const inputClass = `w-full rounded border border-gray-500 px-2 py-2 font-mono`
+
+type LoaderData = {
+  post?: Post
+}
 
 type ActionData = {
   title: string | null
@@ -17,10 +22,10 @@ type ActionData = {
 export const loader: LoaderFunction = async ({ request, params }) => {
   await requireAdminUser(request)
 
-  if (!params?.slug || params?.slug === 'new') return json({})
+  if (!params?.slug || params?.slug === 'new') return json<LoaderData>({})
 
   const post = await getPost(params.slug)
-  return json({ post })
+  return json<LoaderData>({ post })
 }
 
 export const action: ActionFunction = async ({ request, params }) => {
@@ -58,7 +63,7 @@ export const action: ActionFunction = async ({ request, params }) => {
 }
 
 export default function NewPostRoute() {
-  const data = useLoaderData()
+  const data = useLoaderData() as LoaderData
   const errors = useActionData() as ActionData
   const transition = useTransition()
 
